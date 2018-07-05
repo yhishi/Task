c8b39792777646e62ecd61a2097e7313a27a3600
yhishi <hishikawakeiki@hishi-Mac.local>
2018-07-06 00:06:31 +0900

    登録・更新・削除処理追加

--- app/src/main/java/com/yhishi/mytask/MainActivity.kt
+++ app/src/main/java/com/yhishi/mytask/MainActivity.kt
@@ -1,13 +1,21 @@
 package com.yhishi.mytask
 
 import android.os.Bundle
+import android.support.v4.content.ContextCompat.startActivity
 import android.support.v7.app.AppCompatActivity
+import android.view.View
+import android.widget.AdapterView
+import android.widget.ListView
+import android.widget.Toast
 import io.realm.Realm
 import io.realm.kotlin.where
 
 import kotlinx.android.synthetic.main.activity_main.*
 import kotlinx.android.synthetic.main.content_main.*
 import org.jetbrains.anko.startActivity
+import com.yhishi.mytask.R.id.listView
+
+
 
 class MainActivity : AppCompatActivity() {
 
@@ -32,6 +40,13 @@ class MainActivity : AppCompatActivity() {
         // タスク編集画面に遷移
         fab.setOnClickListener { view ->
             startActivity<TaskEditActivity>()
+        }
+
+        // 詳細画面遷移
+        listView.setOnItemClickListener { parent, view, position, id -> // SAM変換
+            // タップしたタスクのデータ位置取得・編集画面に渡す
+            val task = parent.getItemAtPosition(position) as Task
+            startActivity<TaskEditActivity>("task_id" to task.id)
         }
     }
 
--- app/src/main/java/com/yhishi/mytask/TaskEditActivity.kt
+++ app/src/main/java/com/yhishi/mytask/TaskEditActivity.kt
@@ -2,11 +2,118 @@ package com.yhishi.mytask
 
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import io.realm.Realm
+import io.realm.kotlin.createObject
+import io.realm.kotlin.where
+import kotlinx.android.synthetic.main.activity_task_edit.*
+import org.jetbrains.anko.alert
+import org.jetbrains.anko.yesButton
+import java.text.SimpleDateFormat
+import java.util.Date
+import java.text.ParseException
+import android.text.format.DateFormat
+import android.view.View
 
 class TaskEditActivity : AppCompatActivity() {
+
+    private lateinit var realm: Realm
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_task_edit)
+        realm = Realm.getDefaultInstance()
+
+        // インテントに格納したtask_idを取得（取得できなかった場合つまり新規登録の場合は-1）
+        val taskId = intent?.getLongExtra("task_id", -1L)
+
+        // 更新の場合
+        if (taskId != -1L) {
+            // 受け取ったtaskIdに一致するidをrealmから検索
+            val task = realm.where<Task>().equalTo("id", taskId).findFirst()
+
+            // 画面に表示
+            dateEdit.setText(DateFormat.format("yyyy/MM/dd", task?.date))
+            titleEdit.setText(task?.title)
+            detailEdit.setText(task?.detail)
+
+            // 削除ボタンを表示
+            delete.visibility = View.VISIBLE
+        } else {
+            // 削除ボタンを非表示
+            delete.visibility = View.INVISIBLE
+        }
+
+        // 保存処理
+        save.setOnClickListener {
+            when(taskId) {
+                // 新規登録の場合
+                -1L -> {
+                    // realmトランザクション実行
+                    realm.executeTransaction {
+                        val maxId = realm.where<Task>().max("id")
+
+                        // maxIdがnot nullならlong変換, nullなら0
+                        val nextId = (maxId?.toLong() ?: 0L) + 1
+
+                        // データを一行追加
+                        val task = realm.createObject<Task>(nextId)
+
+                        // 入力された日付をDate型に変換後、設定
+                        dateEdit.text.toString().toDate("yyyy/MM/dd")?.let {
+                            task.date = it
+                        }
+                        task.title = titleEdit.text.toString()
+                        task.detail = detailEdit.text.toString()
+                    }
+                    alert("追加しました！") {
+                        yesButton { finish() }
+                    }.show()
+                }
+                // 更新の場合
+                else -> {
+                    realm.executeTransaction {
+                        val task = realm.where<Task>().equalTo("id", taskId).findFirst()
+                        dateEdit.text.toString().toDate("yyyy/MM/dd")?.let {
+                            task?.date = it
+                        }
+                        task?.title = titleEdit.text.toString()
+                        task?.detail = detailEdit.text.toString()
+                    }
+                    alert("修正しました！") {
+                        yesButton { finish() }
+                    }.show()
+                }
+            }
+        }
+        // 削除処理
+        delete.setOnClickListener {
+            realm.executeTransaction {
+                realm.where<Task>().equalTo("id", taskId)?.findFirst()?.deleteFromRealm()
+            }
+            alert("削除しました！") {
+                yesButton { finish() }
+            }.show()
+        }
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+        realm.close()
+    }
+
+    fun String.toDate(pattern: String = "yyyy/MM/dd HH:mm"): Date? {
+        val sdFormat = try {
+            SimpleDateFormat(pattern)
+        } catch (e: IllegalArgumentException) {
+            null
+        }
+        val date = sdFormat?.let {
+            try {
+                it.parse(this)
+            } catch (e: ParseException) {
+                null
+            }
+        }
+        return date
     }
 }
